0d07313b6744c12b8ff20ac1b2b03fd5bc317582
davidasix <danderson@bigriveranalytics.com>
2025-01-25 22:38:02 -0500

    Utilized new logic abstractions instead of embedding in endpoint. Added utility function to fetch the latest record of an event

--- src/app/api/fetch-updated-data/route.ts
+++ src/app/api/fetch-updated-data/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 import { z } from "zod";
-import { eq, desc, and } from "drizzle-orm";
-import { db } from "@/schema/db";
-import { events } from "@/schema/crud";
-import axios from "axios";
+import { getLastEvent } from "../utils";
+import { updateReviews } from "../google/update-reviews";
+import { updateBusinessStats } from "../google/update-business-stats";
+import { fetchReviews } from "../google/fetch-reviews";
+import { fetchBusinessStats } from "../google/fetch-business-stats";
 
 const bodySchema = z.object({
   business_id: z.number(),
@@ -11,7 +12,7 @@ const bodySchema = z.object({
 
 /**
  * Checks if reviews/stats need updating, updates if needed, then returns latest data
- * 
+ *
  * @param { business_id: number } - The database ID of the business
  * @returns Latest reviews and stats for the business
  */
@@ -31,50 +32,25 @@ export async function POST(req: Request) {
     oneDayAgo.setDate(oneDayAgo.getDate() - 1);
 
     // Check last update times
-    const lastUpdateReviews = await db
-      .select()
-      .from(events.table)
-      .where(
-        and(
-          eq(events.table.business_id, business_id),
-          eq(events.table.event, "update_reviews")
-        )
-      )
-      .orderBy(desc(events.table.timestamp))
-      .limit(1)
-      .then((rows) => rows[0]);
-
-    const lastUpdateStats = await db
-      .select()
-      .from(events.table)
-      .where(
-        and(
-          eq(events.table.business_id, business_id),
-          eq(events.table.event, "update_stats")
-        )
-      )
-      .orderBy(desc(events.table.timestamp))
-      .limit(1)
-      .then((rows) => rows[0]);
+    const lastUpdateReviews = await getLastEvent("update_reviews", business_id);
+    const lastUpdateStats = await getLastEvent("update_stats", business_id);
 
     // If data is out of date, fetch and update
     if (
       !lastUpdateReviews?.timestamp ||
       lastUpdateReviews.timestamp < oneDayAgo
     ) {
-      await axios.post("/api/update-reviews", { business_id });
+      await updateReviews(business_id);
     }
 
     if (!lastUpdateStats?.timestamp || lastUpdateStats.timestamp < oneDayAgo) {
-      await axios.post("/api/update-business-stats", { business_id });
+      await updateBusinessStats(business_id);
     }
 
     // Get the data
     const [reviews, stats] = await Promise.all([
-      axios.post("/api/fetch-reviews", { business_id }).then((r) => r.data),
-      axios
-        .post("/api/fetch-business-stats", { business_id })
-        .then((r) => r.data),
+      fetchReviews(business_id),
+      fetchBusinessStats(business_id),
     ]);
 
     return NextResponse.json({
--- src/app/api/utils.ts
+++ src/app/api/utils.ts
@@ -1,3 +1,4 @@
+import { and, eq, desc } from "drizzle-orm";
 import { db } from "@/schema/db";
 import { type DBEvent } from "@/schema/schema";
 import { events } from "@/schema/crud";
@@ -9,4 +10,21 @@ export async function recordEvent(event: DBEvent, business_id: number) {
   };
 
   await db.insert(events.table).values(eventData);
+}
+
+export async function getLastEvent(event: DBEvent, business_id: number) {
+  const lastEvent = await db
+    .select()
+    .from(events.table)
+    .where(
+      and(
+        eq(events.table.business_id, business_id),
+        eq(events.table.event, event)
+      )
+    )
+    .orderBy(desc(events.table.timestamp))
+    .limit(1)
+    .then((rows) => rows[0]);
+
+  return lastEvent;
 }
