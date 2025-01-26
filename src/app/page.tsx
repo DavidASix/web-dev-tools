a0d3224ce8413aee958ccd44ba16aaba44423be7
davidasix <danderson@bigriveranalytics.com>
2025-01-26 09:51:15 -0500

    Added Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Navigation from "@/components/structure/Navigation";
+
 export default function Home() {
   return (
     <div>
+      <Navigation />
       <h1> Hello World </h1>
       <h2> This web app allows developers to display Google Reviews in an easy predictable way.</h2>
     </div>
