07f4971e48b85c3cb3f0d806b9a7d88a5630754d
davidasix <danderson@bigriveranalytics.com>
2025-01-25 19:16:02 -0500

    Added endpoint to fetch the latest  business stats

---
+++ src/app/api/fetch-business-stats/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+import { z } from "zod";
+import { eq, desc } from "drizzle-orm";
+
+import { db } from "@/schema/db";
+import { businessStats } from "@/schema/crud";
+
+const bodySchema = z.object({
+  business_id: z.number(),
+});
+
+/**
+ * Fetch the latest business stats for a given business ID
+ * @param { business_id: number } - The database ID of the business to fetch stats for
+ * @returns { review_count: number, review_score: number } - The latest stats for the given business
+ */
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { business_id } = bodySchema.parse(body);
+
+    if (!business_id) {
+      return NextResponse.json(
+        { error: "Business ID is required" },
+        { status: 400 }
+      );
+    }
+
+    const latestStats = await db
+      .select({
+        review_count: businessStats.table.review_count,
+        review_score: businessStats.table.review_score,
+      })
+      .from(businessStats.table)
+      .where(eq(businessStats.table.business_id, business_id))
+      .orderBy(desc(businessStats.table.created_at))
+      .limit(1)
+      .then((rows) => rows[0]);
+
+    return NextResponse.json(latestStats);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
