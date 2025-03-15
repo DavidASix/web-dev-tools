c55167956ea2bde76a4d9ca90738571143914412
David Anderson <davidandersonsix@gmail.com>
2025-03-15 18:58:33 -0400

    Implemented Holy Loader. Made docker manage its own modules to ensure they're captured on container start

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "tailwindcss/tailwind.css";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+import HolyLoader from "holy-loader";
 
 import { Toaster } from "@/components/ui/sonner";
 
@@ -13,6 +14,7 @@ export default function RootLayout({
     <html lang="en">
       <SessionProvider>
         <body>
+          <HolyLoader />
           {children}
           <Toaster />
         </body>
