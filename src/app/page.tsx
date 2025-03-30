d48114a68267b5b8d60b5d9c13fac1b8450136c0
davidasix <danderson@bigriveranalytics.com>
2025-03-30 12:42:33 -0400

    Made homepage redirect to dashboard for authed users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,26 @@
 import React from "react";
 import type { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 import { auth } from "~/auth";
 import Landing from "./(site)/Landing";
-import Dashboard from "./(product)/Dashboard";
-
-const productMetadata: Metadata = {
-  title: "Admin | Google Reviews for Developers",
-};
 
 const siteMetadata: Metadata = {
   title: "Google Reviews for Developers",
 };
 
 export async function generateMetadata(): Promise<Metadata> {
-  try {
-    const session = await auth();
-    if (session) {
-      return productMetadata;
-    } else {
-      return siteMetadata;
-    }
-  } catch {
-    return siteMetadata;
-  }
+  return siteMetadata;
 }
 
 export default async function Home() {
-  try {
-    const session = await auth();
-    if (session) {
-      return <Dashboard />;
-    } else {
-      return <Landing />;
-    }
-  } catch {
-    return <Landing />;
+  const session = await auth();
+  console.log("Session:", session);
+  
+  if (session) {
+    console.log('Session exists, redirecting to dashboard');
+    redirect("/dashboard");
   }
+
+  return <Landing />;
 }
