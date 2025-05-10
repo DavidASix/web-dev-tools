e786da56de6782a4f9aab9c3d651d82165639bf5
davidasix <danderson@bigriveranalytics.com>
2025-05-10 09:39:17 -0400

    withAuth accepts more args

--- src/middleware/withAuth.ts
+++ src/middleware/withAuth.ts
@@ -6,15 +6,15 @@ import { User } from "next-auth";
 /**
  * Middleware wrapper to ensure that the user is authenticated before proceeding with the request.
  */
-export function withAuth(
-  handler: (req: NextRequest, user: User) => Promise<Response>
+export function withAuth<T extends unknown[]>(
+  handler: (req: NextRequest, user: User, ...args: T) => Promise<Response>
 ) {
-  return async function (req: NextRequest): Promise<Response> {
+  return async function (req: NextRequest, ...args: T): Promise<Response> {
     // Get the auth session
     const session = await auth();
     if (!session?.user?.id) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-    return handler(req, session.user);
+    return handler(req, session.user, ...args);
   };
 }
