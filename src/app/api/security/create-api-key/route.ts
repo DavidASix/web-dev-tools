bb885989dc4f1e96d5b7655aaab5038900a7a292
davidasix <danderson@bigriveranalytics.com>
2025-05-16 10:54:41 -0400

    Implemented handler

--- src/app/api/security/create-api-key/route.ts
+++ src/app/api/security/create-api-key/route.ts
@@ -3,27 +3,32 @@ import { NextResponse } from "next/server";
 import { eq } from "drizzle-orm";
 import schema from "./schema";
 
+import { NextRouteContext, RequestHandler } from "@/middleware/types";
+import { withAuth } from "@/middleware/withAuth";
+
 import { apiKeys } from "@/schema/crud";
 import { db } from "@/schema/db";
 import { generateApiKey } from "@/lib/server/api-keys";
-import { withAuth } from "@/middleware/withAuth";
 
-export const GET = withAuth(async (_, user_id) => {
-  try {
-    await db
-      .update(apiKeys.table)
-      .set({ expired: true })
-      .where(eq(apiKeys.table.user_id, user_id));
+export const GET: RequestHandler<NextRouteContext> = withAuth(
+  async (_, context) => {
+    const { user_id } = context;
+    try {
+      await db
+        .update(apiKeys.table)
+        .set({ expired: true })
+        .where(eq(apiKeys.table.user_id, user_id));
 
-    const key = await generateApiKey(user_id);
+      const key = await generateApiKey(user_id);
 
-    schema.response.parse(key);
-    return NextResponse.json({ key }, { status: 200 });
-  } catch (error) {
-    console.error("Error processing request:", error);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
+      schema.response.parse(key);
+      return NextResponse.json({ key }, { status: 200 });
+    } catch (error) {
+      console.error("Error processing request:", error);
+      return NextResponse.json(
+        { error: "Internal Server Error" },
+        { status: 500 }
+      );
+    }
   }
-});
+);
